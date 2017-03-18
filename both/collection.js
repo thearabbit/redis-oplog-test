a0e38973c8687f19f4fc5a34276f1cab4b860c7d
Theara <yuom.theara@gmail.com>
2017-03-18 08:56:00 +0700

    update

--- both/collection.js
+++ both/collection.js
@@ -1,3 +1,9 @@
-import {Mongo} from 'meteor/mongo';
+import { Mongo } from 'meteor/mongo';
 
 Messages = new Mongo.Collection('messages');
+
+Messages.allow({
+    insert: () => true,
+    update: () => true,
+    remove: () => true,
+});
